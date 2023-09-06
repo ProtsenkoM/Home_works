4c3236115922505538b7e64c65a06fcdbdc4e0a6
ProtsenkoM <mishunya4@gmail.com>
2023-09-06 10:42:07 +0300

    task_3_5

--- homework_task_4/task_5.py
+++ homework_task_4/task_5.py
@@ -2,5 +2,11 @@
 5-
 The list of names is given: names = ['Jack', 'Leon', 'Alice', None, 32, 'Bob']
 Using the continue statement, print only the correct names to the console
-
 """
+
+list_of_names = ['Jack', 'Leon', 'Alice', None, 32, 'Bob']
+
+for name in list_of_names:
+    if not isinstance(name, str):
+        continue
+    print(name)
