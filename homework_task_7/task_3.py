3a5fcd4dd620d79093ff2baf658ab1f3bcc3962b
ProtsenkoM <mishunya4@gmail.com>
2023-09-16 10:10:22 +0300

    corrections

--- homework_task_7/task_3.py
+++ homework_task_7/task_3.py
@@ -8,18 +8,18 @@ xxxxx"""
 
 
 def display_box(width: int, height: int, character="*"):
-    if width <=0 or height <=0:
+    if width <= 0 or height <= 0:
         print("Невірні розміри прямокутника")
 
     for i in range(height):
         if i == 0 or i == height - 1:
-            print (width * character)
+            print(width * character)
         else:
             print(character + ' ' * (width - 2) + character)
 
 
-if __name__=='__main__':
-   display_box(5, 4, 'x') 
-   display_box(15, 8, 'x')
-   display_box(25, 15, 'x')
-   display_box(0, 15, 'x')
+if __name__ == '__main__':
+    display_box(5, 4, 'x')
+    display_box(15, 8, 'x')
+    display_box(25, 15, 'x')
+    display_box(0, 15, 'x')
